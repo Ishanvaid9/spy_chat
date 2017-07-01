ecabe5b15156ffcab0f39095e9fafd7e0cc97fcb
Ishan <vaidishan9@gmail.com>
2017-07-01 11:26:32 +0530

    hello

---
+++ HELLO WORLD.py
@@ -0,0 +1,40 @@
+print 'hello world',
+'''print'scond line'\
+    "IHJDSK"
+a=10
+b=20
+print 'sum is' ,a+b
+print "hello"
+#ishan
+"""
+print'ishan"""
+num=0
+if num>0:
+    print 'positive'
+elif num==0 :
+    print'hell'
+else :
+    print'negative'
+mi =int(raw_input("enter number:"))
+print mi
+print len('\w')
+
+def add(a=3,b=2):
+    var =a+b
+    return  var
+print add(b)
+x=5
+y=x+3
+y=int(str(y)+"2")
+print(y)
+if not True:
+    print("1")
+elif not(1+1==3):
+    print('2')
+else:
+    print ('3')'''
+if 1+1*3==6:
+    print('yes')
+else:
+    print('no')
+
